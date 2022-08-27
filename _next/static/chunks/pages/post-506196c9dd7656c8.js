9bc3b0c3682a7ba184c2905feb5bcf1e3fc9ea6b
shinhu <shinhu91@gmail.com>
2022-08-27 19:06:40 +0900

    Updates

---
+++ _next/static/chunks/pages/post-506196c9dd7656c8.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[86],{830:function(a,b,c){(window.__NEXT_P=window.__NEXT_P||[]).push(["/post",function(){return c(3656)}])},3656:function(a,b,c){"use strict";c.r(b);var d=c(5944);c(7294);var e=function(){return(0,d.tZ)("div",{})};b.default=e}},function(a){a.O(0,[774,888,179],function(){var b;return a(a.s=830)}),_N_E=a.O()}])
