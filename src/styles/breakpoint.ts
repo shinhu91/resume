0bf41728418ecb9315f81b2d533d399be3ce65cb
shinhu <shinhu91@gmail.com>
2022-09-03 12:21:03 +0900

    Updates

--- src/styles/breakpoint.ts
+++
@@ -1,9 +0,0 @@
-const breakpoints = [576, 768, 992, 1200];
-
-export const mqMinWidth = breakpoints.map(
-  (bp) => `@media (min-width: ${bp}px)`
-);
-
-export const mqMaxWidth = breakpoints.map(
-  (bp) => `@media (max-width: ${bp}px)`
-);
--- src/styles/reset.ts
+++
@@ -1,8 +0,0 @@
-import emotionReset from "emotion-reset";
-import { css } from "@emotion/react";
-
-const reset = css`
-  ${emotionReset}
-`;
-
-export default reset;
